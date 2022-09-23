0908a88c7fd06795ffd1f5539fc455e13470487b
barbara73 <barbara.jesacher@gmail.com>
2022-09-23 09:06:25 +0200

    correction - tests not yet implemented

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -1,13 +1,3 @@
-from src.logdecoratorandhandler.log_decorator import LogDecorator
 from src.logdecoratorandhandler.options import Options
 
 Options.log_level = 'DEBUG'
-
-
-@LogDecorator('DEBUG - test a function')
-def a_function():
-    return
-
-
-if __name__ == '__main__':
-    a_function()
